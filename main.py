3c91d8289800b422e13652d53a884d334775819d
sien08 <130815882+sien08@users.noreply.github.com>
2026-04-09 18:46:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -39,4 +39,8 @@ def webhook():
 
     return "ok"
 
-app.run(port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
